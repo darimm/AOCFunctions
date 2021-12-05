cc59639829b83b495f87ccbc68f806df588a9a59
Scott Stanley <darimm@gmail.com>
2021-12-05 10:14:13 -0500

    Initial Commit

---
+++ AOCFunctions.go
@@ -0,0 +1,38 @@
+package AOCFunctions
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+func MinInt(vars ...int) int {
+	min := vars[0]
+
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+
+	return min
+}
+
+func Maxint(vars ...int) int {
+	max := vars[0]
+
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+
+	return max
+}
+
+func ReadFile(fileName string) ([]string, error) {
+	fileBytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(fileBytes), "\n"), nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/darimm/AOC2020/AOCFunctions
+
+go 1.17
